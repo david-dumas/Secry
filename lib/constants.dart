c127c0d9a22b6945bcacf8ce9fe798eee866c2fb
Twan <twanvandenbor@hotmail.com>
2021-12-03 11:43:06 +0100

    US-53: Add constants file with global colors and text styles

---
+++ lib/constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// Most used color(s)
+const kPrimaryColor = Color(0xFF3794FC);
+
+// Colors for tabbar and separators
+const kBottomMenuUnselectedColor = Color(0xFFB9BEBC);
+const kLineSeparatorColor = Color(0xFFDEE5EF);
+
+// Colors for buttons and input field
+const kWhiteButtonBorderColor = Color(0xFFDEE5EF);
+const kWhiteInputFieldBorderStyle = Color(0xFFD7D7D7);
+
+// Gray tints
+const globalBlack = Color(0xFF000000);
+const kDarkGrayTextColor = Color(0xFF3F3F3F);
+const kDarkGray = Color(0xFF4F4F4F);
+const kMediumGray = Color(0xFFB3B9BC);
+const kLightGray = Color(0xFFF4F4F4);
+const globalWhite = Color(0xFFFFFFFF);
+
+// Text styles
+const kWalkthroughTitleTextStyle = TextStyle(
+  color: kPrimaryColor,
+  fontSize: 21.0,
+);
+const kWalkthroughDescriptionTextStyle = TextStyle(
+  color: kDarkGrayTextColor,
+  fontSize: 16.0,
+);
