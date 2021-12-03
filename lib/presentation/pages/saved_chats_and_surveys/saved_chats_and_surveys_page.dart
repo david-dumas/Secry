acca4b887a49a9b0b7ab1da6c9e92c2ed425c229
Twan <twanvandenbor@hotmail.com>
2021-12-03 11:45:04 +0100

    US-53: Add empty global pages

---
+++ lib/presentation/pages/saved_chats_and_surveys/saved_chats_and_surveys_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SavedChatsAndSurveysPage extends StatelessWidget {
+  const SavedChatsAndSurveysPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
