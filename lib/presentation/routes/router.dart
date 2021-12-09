f595f4f63db364b83d1290d37453ff7e64a0485a
Twan <twanvandenbor@hotmail.com>
2021-12-09 11:41:33 +0100

    US-130 Use SplashPage in router

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -3,10 +3,12 @@ import 'package:secry/presentation/pages/account/account_page.dart';
 import 'package:secry/presentation/pages/home/homepage.dart';
 import 'package:secry/presentation/pages/saved_chats_and_surveys/saved_chats_and_surveys_page.dart';
 import 'package:secry/presentation/pages/search/global_search_page.dart';
+import 'package:secry/presentation/pages/splash/splash_page.dart';
 import 'package:secry/presentation/pages/tabbar/tabbar_page.dart';
 
 @MaterialAutoRouter(routes: [
-  MaterialRoute(page: TabbarPage, initial: true),
+  MaterialRoute(page: SplashPage, initial: true),
+  MaterialRoute(page: TabbarPage),
   MaterialRoute(page: HomePage),
   MaterialRoute(page: GlobalSearchPage),
   MaterialRoute(page: SavedChatsAndSurveysPage),
--- lib/presentation/routes/router.gr.dart
+++ lib/presentation/routes/router.gr.dart
@@ -8,87 +8,100 @@
 // AutoRouteGenerator
 // **************************************************************************
 
-import 'package:auto_route/auto_route.dart' as _i6;
-import 'package:flutter/material.dart' as _i7;
+import 'package:auto_route/auto_route.dart' as _i7;
+import 'package:flutter/material.dart' as _i8;
 
-import '../pages/account/account_page.dart' as _i5;
-import '../pages/home/homepage.dart' as _i2;
+import '../pages/account/account_page.dart' as _i6;
+import '../pages/home/homepage.dart' as _i3;
 import '../pages/saved_chats_and_surveys/saved_chats_and_surveys_page.dart'
-    as _i4;
-import '../pages/search/global_search_page.dart' as _i3;
-import '../pages/tabbar/tabbar_page.dart' as _i1;
+    as _i5;
+import '../pages/search/global_search_page.dart' as _i4;
+import '../pages/splash/splash_page.dart' as _i1;
+import '../pages/tabbar/tabbar_page.dart' as _i2;
 
-class AppRouter extends _i6.RootStackRouter {
-  AppRouter([_i7.GlobalKey<_i7.NavigatorState>? navigatorKey])
+class AppRouter extends _i7.RootStackRouter {
+  AppRouter([_i8.GlobalKey<_i8.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i6.PageFactory> pagesMap = {
+  final Map<String, _i7.PageFactory> pagesMap = {
+    SplashPageRoute.name: (routeData) {
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i1.SplashPage());
+    },
     TabbarPageRoute.name: (routeData) {
-      return _i6.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i1.TabbarPage());
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.TabbarPage());
     },
     HomePageRoute.name: (routeData) {
-      return _i6.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i2.HomePage());
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i3.HomePage());
     },
     GlobalSearchPageRoute.name: (routeData) {
-      return _i6.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i3.GlobalSearchPage());
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i4.GlobalSearchPage());
     },
     SavedChatsAndSurveysPageRoute.name: (routeData) {
-      return _i6.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i4.SavedChatsAndSurveysPage());
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i5.SavedChatsAndSurveysPage());
     },
     AccountPageRoute.name: (routeData) {
-      return _i6.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i5.AccountPage());
+      return _i7.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i6.AccountPage());
     }
   };
 
   @override
-  List<_i6.RouteConfig> get routes => [
-        _i6.RouteConfig(TabbarPageRoute.name, path: '/'),
-        _i6.RouteConfig(HomePageRoute.name, path: '/home-page'),
-        _i6.RouteConfig(GlobalSearchPageRoute.name,
+  List<_i7.RouteConfig> get routes => [
+        _i7.RouteConfig(SplashPageRoute.name, path: '/'),
+        _i7.RouteConfig(TabbarPageRoute.name, path: '/tabbar-page'),
+        _i7.RouteConfig(HomePageRoute.name, path: '/home-page'),
+        _i7.RouteConfig(GlobalSearchPageRoute.name,
             path: '/global-search-page'),
-        _i6.RouteConfig(SavedChatsAndSurveysPageRoute.name,
+        _i7.RouteConfig(SavedChatsAndSurveysPageRoute.name,
             path: '/saved-chats-and-surveys-page'),
-        _i6.RouteConfig(AccountPageRoute.name, path: '/account-page')
+        _i7.RouteConfig(AccountPageRoute.name, path: '/account-page')
       ];
 }
 
-/// generated route for [_i1.TabbarPage]
-class TabbarPageRoute extends _i6.PageRouteInfo<void> {
-  const TabbarPageRoute() : super(name, path: '/');
+/// generated route for [_i1.SplashPage]
+class SplashPageRoute extends _i7.PageRouteInfo<void> {
+  const SplashPageRoute() : super(name, path: '/');
+
+  static const String name = 'SplashPageRoute';
+}
+
+/// generated route for [_i2.TabbarPage]
+class TabbarPageRoute extends _i7.PageRouteInfo<void> {
+  const TabbarPageRoute() : super(name, path: '/tabbar-page');
 
   static const String name = 'TabbarPageRoute';
 }
 
-/// generated route for [_i2.HomePage]
-class HomePageRoute extends _i6.PageRouteInfo<void> {
+/// generated route for [_i3.HomePage]
+class HomePageRoute extends _i7.PageRouteInfo<void> {
   const HomePageRoute() : super(name, path: '/home-page');
 
   static const String name = 'HomePageRoute';
 }
 
-/// generated route for [_i3.GlobalSearchPage]
-class GlobalSearchPageRoute extends _i6.PageRouteInfo<void> {
+/// generated route for [_i4.GlobalSearchPage]
+class GlobalSearchPageRoute extends _i7.PageRouteInfo<void> {
   const GlobalSearchPageRoute() : super(name, path: '/global-search-page');
 
   static const String name = 'GlobalSearchPageRoute';
 }
 
-/// generated route for [_i4.SavedChatsAndSurveysPage]
-class SavedChatsAndSurveysPageRoute extends _i6.PageRouteInfo<void> {
+/// generated route for [_i5.SavedChatsAndSurveysPage]
+class SavedChatsAndSurveysPageRoute extends _i7.PageRouteInfo<void> {
   const SavedChatsAndSurveysPageRoute()
       : super(name, path: '/saved-chats-and-surveys-page');
 
   static const String name = 'SavedChatsAndSurveysPageRoute';
 }
 
-/// generated route for [_i5.AccountPage]
-class AccountPageRoute extends _i6.PageRouteInfo<void> {
+/// generated route for [_i6.AccountPage]
+class AccountPageRoute extends _i7.PageRouteInfo<void> {
   const AccountPageRoute() : super(name, path: '/account-page');
 
   static const String name = 'AccountPageRoute';
