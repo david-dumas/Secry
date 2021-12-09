464d35c09a3ebe785f9eafa0361aae4f15705736
Twan <twanvandenbor@hotmail.com>
2021-12-09 11:40:52 +0100

    US-130 update injection config (from build runner)

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -8,8 +8,9 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import 'application/main/main_bloc.dart' as _i3;
+import 'application/splash/splash_bloc.dart' as _i4;
 import 'application/tabbar/tabbar_bloc.dart'
-    as _i4; // ignore_for_file: unnecessary_lambdas
+    as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -17,6 +18,7 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   gh.factory<_i3.MainBloc>(() => _i3.MainBloc());
-  gh.factory<_i4.TabbarBloc>(() => _i4.TabbarBloc());
+  gh.factory<_i4.SplashBloc>(() => _i4.SplashBloc());
+  gh.factory<_i5.TabbarBloc>(() => _i5.TabbarBloc());
   return get;
 }
