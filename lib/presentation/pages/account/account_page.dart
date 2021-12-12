6b7b090f05272b40645db2943b4b9e6c00376a72
Clint Siadis <clint-siadis@hotmail.com>
2021-12-13 00:46:32 +0100

    WIP: Play with layout components

--- lib/presentation/pages/account/account_page.dart
+++ lib/presentation/pages/account/account_page.dart
@@ -6,16 +6,42 @@ class AccountPage extends StatelessWidget {
   const AccountPage({Key? key}) : super(key: key);
 
   @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: globalWhite,
+  //     body: Center(
+  //       child: Text(
+  //         'How well do you really know those around you? Secry is about the real world! Create a group with people you know and ask them anything. Talk about issues, insecurities, sex, work, anthing you want to talk about. Become aware, help each other, connect and discover secrets!',
+  //         textAlign: TextAlign.center,
+  //       ),
+  //     ),
+  //     // body: Center(
+  //     //   child: Container(
+  //     //     padding: const EdgeInsets.all(80),
+  //     //     child: Text(
+  //     //       'How well do you really know those around you? Secry is about the real world! Create a group with people you know and ask them anything. Talk about issues, insecurities, sex, work, anthing you want to talk about. Become aware, help each other, connect and discover secrets!',
+  //     //       textAlign: TextAlign.center,
+  //     //       // overflow: TextOverflow.ellipsis,
+  //     //       // style: const TextStyle(fontWeight: FontWeight.bold),
+  //     //     ),
+
+  //     //   ),
+  //     // ),
+  //   );
+  // }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: globalWhite,
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(32),
-          child: Image(
-            image: AssetImage('assets/launcher/foreground.png'),
-          ),
-        ),
+      body: Column(
+        children: [
+          TextButton(
+            child: Text('Woolha.com'),
+            onPressed: () {
+              print('Pressed');
+            },
+          )
+        ],
       ),
     );
   }
