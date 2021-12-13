01ea2e0d7a52014bffcb9a9fe604224db0bda36d
Clint Siadis <clint-siadis@hotmail.com>
2021-12-13 10:12:40 +0100

    Add title en text info in containers

--- lib/presentation/pages/account/account_page.dart
+++ lib/presentation/pages/account/account_page.dart
@@ -6,42 +6,30 @@ class AccountPage extends StatelessWidget {
   const AccountPage({Key? key}) : super(key: key);
 
   @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: globalWhite,
-  //     body: Center(
-  //       child: Text(
-  //         'How well do you really know those around you? Secry is about the real world! Create a group with people you know and ask them anything. Talk about issues, insecurities, sex, work, anthing you want to talk about. Become aware, help each other, connect and discover secrets!',
-  //         textAlign: TextAlign.center,
-  //       ),
-  //     ),
-  //     // body: Center(
-  //     //   child: Container(
-  //     //     padding: const EdgeInsets.all(80),
-  //     //     child: Text(
-  //     //       'How well do you really know those around you? Secry is about the real world! Create a group with people you know and ask them anything. Talk about issues, insecurities, sex, work, anthing you want to talk about. Become aware, help each other, connect and discover secrets!',
-  //     //       textAlign: TextAlign.center,
-  //     //       // overflow: TextOverflow.ellipsis,
-  //     //       // style: const TextStyle(fontWeight: FontWeight.bold),
-  //     //     ),
-
-  //     //   ),
-  //     // ),
-  //   );
-  // }
-
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: globalWhite,
-      body: Column(
-        children: [
-          TextButton(
-            child: Text('Woolha.com'),
-            onPressed: () {
-              print('Pressed');
-            },
-          )
-        ],
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 32),
+              child: Text(
+                'Join the community!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 32, right: 32),
+              //margin: EdgeInsets.only(bottom: 32),
+              child: Text(
+                'How well do you really know those around you? Secry is about the real world! Create a group with people you know and ask them anything. Talk about issues, insecurities, sex, work, anthing you want to talk about. Become aware, help each other, connect and discover secrets!',
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
