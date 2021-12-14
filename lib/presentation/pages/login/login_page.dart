7f78f7555866b98f56a1e74f8b982f2711df0fe5
Clint Siadis <clint-siadis@hotmail.com>
2021-12-14 03:00:31 +0100

    Add scaffold text and button in sign in page

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -13,19 +13,47 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    const pagePadding = EdgeInsets.only(left: 75, right: 75, bottom: 32);
+
     return Scaffold(
       backgroundColor: globalWhite,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
               margin: EdgeInsets.only(bottom: 32),
               child: Text(
-                tr('account_logged_out_overview_title'),
+                tr('account_login_title'),
                 textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+              ),
+            ),
+            Container(
+              padding: pagePadding,
+              child: Text(
+                tr('account_logged_out_overview_description'),
+                textAlign: TextAlign.center,
+                style: mainContentTextStyleMedium,
+              ),
+            ),
+            Container(
+              padding: pagePadding,
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  tr('action_login'),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
               ),
             ),
           ],
