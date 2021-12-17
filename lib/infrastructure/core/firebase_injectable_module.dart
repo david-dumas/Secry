b343b97bca4972d6f1d4c3eff8edf698bb1cff15
Twan <twanvandenbor@hotmail.com>
2021-12-17 10:17:03 +0100

    US-56 Setup firebase packages for injection

---
+++ lib/infrastructure/core/firebase_injectable_module.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
+
+@module
+abstract class FirebaseInjectableModule {
+  @singleton
+  FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+  @singleton
+  FirebaseAnalytics get analytics => FirebaseAnalytics.instance;
+  @singleton
+  FirebaseCrashlytics get crashlytics => FirebaseCrashlytics.instance;
+}
