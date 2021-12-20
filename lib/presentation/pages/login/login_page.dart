caeb5340c485eb5abe0c522c75db12b4d1eb261c
Twan <twanvandenbor@hotmail.com>
2021-12-20 16:57:08 +0100

    US-56 Add signin to login page

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:secry/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:secry/constants.dart';
 
 class LoginPage extends StatefulWidget {
@@ -13,85 +15,98 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: globalWhite,
-      body: Center(
-        child: Padding(
-          padding: pagePadding,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                tr('account_login_title'),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 26,
-                    color: kPrimaryColor),
-              ),
-              SizedBox(height: 26),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email_outlined),
-                  labelText: tr('account_email'),
-                ),
-              ),
-              SizedBox(height: 12),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock_outline),
-                  labelText: tr('account_password'),
-                ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    tr('account_forgot_your_password'),
-                    textAlign: TextAlign.center,
-                    style: mainContentTextStyleMedium,
-                  ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      tr('action_click_here'),
+    return BlocProvider(
+      create: (context) => SignInFormBloc(),
+      child: BlocBuilder<SignInFormBloc, SignInFormState>(
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: globalWhite,
+            body: Center(
+              child: Padding(
+                padding: pagePadding,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      tr('account_login_title'),
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                        decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26,
+                          color: kPrimaryColor),
+                    ),
+                    SizedBox(height: 26),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email_outlined),
+                        labelText: tr('account_email'),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Container(
-                padding: pagePadding,
-                width: MediaQuery.of(context).size.width * 0.6,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
+                    SizedBox(height: 12),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock_outline),
+                        labelText: tr('account_password'),
                       ),
                     ),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    tr('action_login'),
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          tr('account_forgot_your_password'),
+                          textAlign: TextAlign.center,
+                          style: mainContentTextStyleMedium,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            tr('action_click_here'),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: pagePadding,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          context
+                              .read<SignInFormBloc>()
+                              .add(SignInFormEvent.signInPressed());
+                        },
+                        child: Text(
+                          tr('action_login'),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
