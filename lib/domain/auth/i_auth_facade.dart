29cc2c8a3d511edb55eddd751aa50fd66df4c8fc
Twan <twanvandenbor@hotmail.com>
2021-12-20 13:00:01 +0100

    US-56: Update signIn method in IAuthInterface

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -4,9 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'auth_failure.dart';
 
 abstract class IAuthFacade {
-  Future<Either<AuthFailure, Unit>> signIn({
-    required User user,
-  });
+  Future<Either<AuthFailure, Unit>> signIn(
+      {required String email, required String password});
   Option<auth.User> getSignedInUser();
   Future<void> signOut();
 }
