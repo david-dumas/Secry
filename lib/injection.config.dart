fde7c5934354972a51c2e06ddecb6e0267b36d29
Twan <twanvandenbor@hotmail.com>
2021-12-20 12:58:28 +0100

    US-56 Update injections

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -4,21 +4,33 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:firebase_auth/firebase_auth.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'application/main/main_bloc.dart' as _i3;
-import 'application/splash/splash_bloc.dart' as _i4;
-import 'application/tabbar/tabbar_bloc.dart'
-    as _i5; // ignore_for_file: unnecessary_lambdas
+import 'application/auth/sign_in_form/sign_in_form_bloc.dart' as _i7;
+import 'application/main/main_bloc.dart' as _i6;
+import 'application/splash/splash_bloc.dart' as _i8;
+import 'application/tabbar/tabbar_bloc.dart' as _i9;
+import 'domain/auth/i_auth_facade.dart' as _i4;
+import 'infrastructure/auth/firebase_auth_facade.dart' as _i5;
+import 'infrastructure/core/firebase_injectable_module.dart'
+    as _i10; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.factory<_i3.MainBloc>(() => _i3.MainBloc());
-  gh.factory<_i4.SplashBloc>(() => _i4.SplashBloc());
-  gh.factory<_i5.TabbarBloc>(() => _i5.TabbarBloc());
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
+  gh.singleton<_i3.FirebaseAuth>(firebaseInjectableModule.firebaseAuth);
+  gh.singleton<_i4.IAuthFacade>(
+      _i5.FirebaseAuthFacade(get<_i3.FirebaseAuth>()));
+  gh.factory<_i6.MainBloc>(() => _i6.MainBloc());
+  gh.factory<_i7.SignInFormBloc>(() => _i7.SignInFormBloc());
+  gh.factory<_i8.SplashBloc>(() => _i8.SplashBloc());
+  gh.factory<_i9.TabbarBloc>(() => _i9.TabbarBloc());
   return get;
 }
+
+class _$FirebaseInjectableModule extends _i10.FirebaseInjectableModule {}
