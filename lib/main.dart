c029c802075ee4336cb906f9a47afc195721dd5a
Twan <twanvandenbor@hotmail.com>
2021-12-20 12:58:46 +0100

    US-56: Initialize firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:secry/presentation/app_widget.dart';
@@ -9,7 +10,7 @@ Future<void> main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
     await EasyLocalization.ensureInitialized();
-
+    await Firebase.initializeApp();
     await configureInjection(Environment.prod);
   } catch (exception) {
     print(exception);
