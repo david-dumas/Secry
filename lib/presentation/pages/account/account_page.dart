7b13b7b231ddf0fe7b15b30a5a8fa66d422e745a
Twan <twanvandenbor@hotmail.com>
2022-01-11 14:27:17 +0100

    US-56: Center account page content and update button height

--- lib/presentation/pages/account/account_page.dart
+++ lib/presentation/pages/account/account_page.dart
@@ -16,80 +16,85 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return BlocBuilder<TabbarBloc, TabbarState>(
       builder: (mainContext, mainState) {
         return Scaffold(
           appBar: GeneralAppbar(),
-          body: SingleChildScrollView(
+          body: Padding(
+            padding: EdgeInsets.only(bottom: screenHeight * 0.1),
             child: Center(
-              child: Padding(
-                padding: pagePadding,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Image(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      image: AssetImage('assets/launcher/foreground.png'),
-                    ),
-                    Text(
-                      tr('account_logged_out_overview_title'),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                    ),
-                    SizedBox(height: 16),
-                    Text(
-                      tr('account_logged_out_overview_description'),
-                      textAlign: TextAlign.center,
-                      style: mainContentTextStyleMedium,
-                    ),
-                    SizedBox(height: 16),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 44,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: pagePadding,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Image(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        image: AssetImage('assets/launcher/foreground.png'),
+                      ),
+                      Text(
+                        tr('account_logged_out_overview_title'),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        tr('account_logged_out_overview_description'),
+                        textAlign: TextAlign.center,
+                        style: mainContentTextStyleMedium,
+                      ),
+                      SizedBox(height: 16),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
                             ),
                           ),
-                        ),
-                        onPressed: () {
-                          pushNewScreen(
-                            context,
-                            screen: SignupPage(),
-                            withNavBar: true,
-                            pageTransitionAnimation: PageTransitionAnimation.cupertino,
-                          );
-                        },
-                        child: Text(
-                          tr('action_create_account'),
-                          style: buttonTextStyleMedium,
+                          onPressed: () {
+                            pushNewScreen(
+                              context,
+                              screen: SignupPage(),
+                              withNavBar: true,
+                              pageTransitionAnimation: PageTransitionAnimation.cupertino,
+                            );
+                          },
+                          child: Text(
+                            tr('action_create_account'),
+                            style: buttonTextStyleMedium,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 4),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 44,
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                          textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      SizedBox(height: 4),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 60.0,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () {
+                            pushNewScreen(
+                              context,
+                              screen: LoginPage(),
+                              withNavBar: true,
+                              pageTransitionAnimation: PageTransitionAnimation.cupertino,
+                            );
+                          },
+                          child: Text(tr('action_login')),
                         ),
-                        onPressed: () {
-                          pushNewScreen(
-                            context,
-                            screen: LoginPage(),
-                            withNavBar: true,
-                            pageTransitionAnimation: PageTransitionAnimation.cupertino,
-                          );
-                        },
-                        child: Text(tr('action_login')),
                       ),
-                    ),
-                    verticalSafetyScrollOffset
-                  ],
+                      verticalSafetyScrollOffset
+                    ],
+                  ),
                 ),
               ),
             ),
