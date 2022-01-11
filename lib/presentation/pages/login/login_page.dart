7882f296beaad42177f7f54a6c33cdd7414a1c07
Twan <twanvandenbor@hotmail.com>
2022-01-11 14:56:11 +0100

    US-56: Fix overflow issue for LoginPage

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -29,39 +29,40 @@ class _LoginPageState extends State<LoginPage> {
             appBar: GeneralAppbar(
               isSubpage: true,
             ),
-            body: Padding(
-              padding: EdgeInsets.only(bottom: screenHeight * 0.1),
-              child: Center(
-                child: Padding(
-                  padding: pagePadding,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          tr('account_login_title'),
-                          textAlign: TextAlign.left,
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26, color: kPrimaryColor),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: screenHeight * 0.1),
+                child: Center(
+                  child: Padding(
+                    padding: pagePadding,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            tr('account_login_title'),
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26, color: kPrimaryColor),
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 26),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.email_outlined),
-                          labelText: tr('account_email'),
+                        SizedBox(height: 26),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.email_outlined),
+                            labelText: tr('account_email'),
+                          ),
+                          onChanged: (value) => context.read<SignInFormBloc>().add(SignInFormEvent.emailChanged(value)),
                         ),
-                        onChanged: (value) => context.read<SignInFormBloc>().add(SignInFormEvent.emailChanged(value)),
-                      ),
-                      SizedBox(height: 12),
-                      TextFormField(
+                        SizedBox(height: 12),
+                        TextFormField(
                           obscureText: state.isShowingPassword,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.lock_outline),
-                          labelText: tr('account_password'),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.lock_outline),
+                            labelText: tr('account_password'),
                             suffixIcon: IconButton(
                               icon: state.isShowingPassword ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
                               onPressed: () {
@@ -70,51 +71,52 @@ class _LoginPageState extends State<LoginPage> {
                                     .add(SignInFormEvent.isShowingPasswordToggled(!state.isShowingPassword));
                               },
                             ),
-                        ),
-                        onChanged: (value) =>
-                            context.read<SignInFormBloc>().add(SignInFormEvent.passwordChanged(value)),
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              tr('account_forgot_your_password'),
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: 16.0,
-                              ),
-                            ),
                           ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0),
-                        child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 50.0,
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(18.0),
+                          onChanged: (value) =>
+                              context.read<SignInFormBloc>().add(SignInFormEvent.passwordChanged(value)),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                tr('account_forgot_your_password'),
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 16.0,
                                 ),
                               ),
                             ),
-                            onPressed: () {
-                              context.read<SignInFormBloc>().add(SignInFormEvent.signInPressed());
-                            },
-                            child: Text(
-                              tr('action_login'),
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 50.0,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                  ),
+                                ),
+                              ),
+                              onPressed: () {
+                                context.read<SignInFormBloc>().add(SignInFormEvent.signInPressed());
+                              },
+                              child: Text(
+                                tr('action_login'),
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
