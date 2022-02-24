c6ebd4230f53a99603ab532c58f4d671b6827746
Twan <twanvandenbor@hotmail.com>
2022-02-24 13:46:12 +0100

    US-250: Update group_overview_row_info object: use same json key/value pairs as api

--- lib/domain/general/group_overview_row_info.dart
+++ lib/domain/general/group_overview_row_info.dart
@@ -3,22 +3,38 @@ import 'package:flutter_svg/flutter_svg.dart';
 class GroupOverviewRowInfo {
   final String id;
   final String title;
-  final String lastMessage;
-  final String timeIndication;
+  final String? lastChatId;
+  final String? lastMessageSender;
+  final String? lastMessageText;
+  final DateTime? lastMessageTime;
+  final DateTime? createdAt;
   DrawableRoot? svg;
 
   GroupOverviewRowInfo(
       {required this.id,
       required this.title,
-      required this.lastMessage,
-      required this.timeIndication,
+      required this.lastChatId,
+      required this.lastMessageSender,
+      required this.lastMessageText,
+      required this.lastMessageTime,
+      required this.createdAt,
       this.svg = null});
 
   factory GroupOverviewRowInfo.fromJsonMap(Map<String, dynamic> json) {
     return GroupOverviewRowInfo(
-        id: json.containsKey('id') ? json['id'] : '',
+        id: json.containsKey('id') ? json['id'].toString() : '',
         title: json.containsKey('title') ? json['title'] : '',
-        lastMessage: json.containsKey('lastMessage') ? json['lastMessage'] : '',
-        timeIndication: json.containsKey('timeIndication') ? json['timeIndication'] : '');
+        lastChatId: json.containsKey('lastChatId') ? json['lastChatId'].toString() : '',
+        lastMessageSender: json.containsKey('lastMessageSender')
+            ? (json['lastMessageSender'] != null ? json['lastMessageSender'] : '')
+            : '',
+        lastMessageText:
+            json.containsKey('lastMessageText') ? (json['lastMessageText'] != null ? json['lastMessageText'] : '') : '',
+        lastMessageTime: json.containsKey('lastMessageTime')
+            ? (json['lastMessageTime'] != null ? DateTime.parse(json['lastMessageTime']) : null)
+            : null,
+        createdAt: json.containsKey('createdAt')
+            ? (json['createdAt'] != null ? DateTime.parse(json['createdAt']) : null)
+            : null);
   }
 }
