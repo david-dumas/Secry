3961c38fe9c74b6793f510f66495e4c9c03960d2
Twan <twanvandenbor@hotmail.com>
2022-03-14 14:58:30 +0100

    US-239: Add GeneralSurveyInfo object

---
+++ lib/domain/surveys/general_survey_info.dart
@@ -0,0 +1,17 @@
+class GeneralSurveyInfo {
+  final String id;
+  final String title;
+  final DateTime? createdAt;
+
+  GeneralSurveyInfo({required this.id, required this.title, this.createdAt = null});
+
+  factory GeneralSurveyInfo.fromJsonMap(Map<String, dynamic> json) {
+    return GeneralSurveyInfo(
+      id: json.containsKey('id') ? (json['id'] != null ? json['id'] : '') : '',
+      title: json.containsKey('title') ? (json['title'] != null ? json['title'] : '') : '',
+      createdAt: json.containsKey('createdAt')
+          ? (json['createdAt'] != null ? DateTime.fromMillisecondsSinceEpoch(json['createdAt']) : null)
+          : null,
+    );
+  }
+}
