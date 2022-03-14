a4c5a5462c5c855f8e190f6b4dd2b65db1115bb3
Twan <twanvandenbor@hotmail.com>
2022-03-14 14:57:53 +0100

    US-239: Delete mock data chats api functions

--- lib/domain/chats/i_chats_repository.dart
+++ lib/domain/chats/i_chats_repository.dart
@@ -1,5 +1,1 @@
-import 'package:secry/domain/general/group_overview_row_info.dart';
-
-abstract class IChatsRepository {
-  Future<List<GroupOverviewRowInfo>> getMostRecentChatsForGroup({required String groupId});
-}
+abstract class IChatsRepository {}
