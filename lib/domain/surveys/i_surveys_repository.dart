17025411fb484eb39a31fd350f39f459b63432ef
Twan <twanvandenbor@hotmail.com>
2022-03-14 14:58:09 +0100

    US-239: Delete mock data surveys api functions

--- lib/domain/surveys/i_surveys_repository.dart
+++ lib/domain/surveys/i_surveys_repository.dart
@@ -1,5 +1,1 @@
-import 'package:secry/domain/general/group_overview_row_info.dart';
-
-abstract class ISurveysRepository {
-  Future<List<GroupOverviewRowInfo>> getMostRecentSurveysForGroup({required String groupId});
-}
+abstract class ISurveysRepository {}
--- lib/infrastructure/surveys/surveys_repository.dart
+++ lib/infrastructure/surveys/surveys_repository.dart
@@ -11,22 +11,4 @@ class SurveysRepository extends ISurveysRepository {
   final SurveysApiService _SurveysApiService;
 
   SurveysRepository(this._SurveysApiService) : super();
-
-  Future<List<GroupOverviewRowInfo>> getMostRecentSurveysForGroup({required String groupId}) async {
-    try {
-      final response = await _SurveysApiService.api.getMostRecentPrivateSurveysForGroup();
-      final responseStatusCode = response.response.statusCode;
-
-      if (responseStatusCode == 200) {
-        final List<GroupOverviewRowInfo> groupOverviewRowsData =
-            (json.decode(response.data) as List).map((json) => GroupOverviewRowInfo.fromJsonMap(json)).toList();
-        return groupOverviewRowsData;
-      } else {
-        return List.empty();
-      }
-    } catch (error) {
-      print(error);
-      return List.empty();
-    }
-  }
 }
