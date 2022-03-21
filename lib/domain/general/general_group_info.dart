0cea95827b156600566c92dd897b6d8352e299f3
Twan <twanvandenbor@hotmail.com>
2022-03-21 17:12:43 +0100

    US-281: Update default value for totalNumberOfGroups in GeneralGroupInfo

--- lib/domain/general/general_group_info.dart
+++ lib/domain/general/general_group_info.dart
@@ -17,8 +17,7 @@ class GeneralGroupInfo {
     return GeneralGroupInfo(
         pageNumber: json.containsKey('pageNumber') ? (json['pageNumber'] != null ? json['pageNumber'] : 1) : 1,
         totalPages: json.containsKey('totalPages') ? (json['totalPages'] != null ? json['totalPages'] : 1) : 1,
-        totalNumberOfGroups:
-            json.containsKey('totalCount') ? (json['totalCount'] != null ? json['totalCount'] : 10) : 10,
+        totalNumberOfGroups: json.containsKey('totalCount') ? (json['totalCount'] != null ? json['totalCount'] : 0) : 0,
         hasPreviousPage: json.containsKey('hasPreviousPage')
             ? (json['hasPreviousPage'] != null ? json['hasPreviousPage'] : false)
             : false,
