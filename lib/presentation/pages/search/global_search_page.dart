035481483e3708e4987acdbdafe0592b31f0dbc4
Twan <twanvandenbor@hotmail.com>
2022-03-21 17:15:14 +0100

    US-281: Setup basic UI for GlobalSearchPage

--- lib/presentation/pages/search/global_search_page.dart
+++ lib/presentation/pages/search/global_search_page.dart
@@ -3,19 +3,66 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:secry/application/tabbar/tabbar_bloc.dart';
 import 'package:secry/constants.dart';
+import 'package:secry/presentation/pages/general/widgets/search_people_section.dart';
 import 'package:secry/presentation/widgets/bars/general_appbar.dart';
 
-class GlobalSearchPage extends StatelessWidget {
-  const GlobalSearchPage({Key? key}) : super(key: key);
+import 'package:secry/application/global_search/global_search_bloc.dart';
+
+import 'package:secry/injection.dart';
+
+class GlobalSearchPage extends StatefulWidget {
+  GlobalSearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<GlobalSearchPage> createState() => _GlobalSearchPageState();
+}
+
+class _GlobalSearchPageState extends State<GlobalSearchPage> {
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController searchBarSearchAllPeopleTextEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TabbarBloc, TabbarState>(
       builder: (mainContext, mainState) {
-        return Scaffold(
-          appBar: GeneralAppbar(
-            title: tr(mainState.currentTitleTagForSelectedIndex),
-            backgroundColor: globalWhite,
+        return BlocProvider(
+          create: (context) => getIt<GlobalSearchBloc>()..add(const GlobalSearchEvent.initialized()),
+          child: BlocBuilder<GlobalSearchBloc, GlobalSearchState>(
+            builder: (context, state) {
+              return Scaffold(
+                appBar: GeneralAppbar(
+                  title: tr(mainState.currentTitleTagForSelectedIndex),
+                  backgroundColor: globalWhite,
+                ),
+                body: RefreshIndicator(
+                  onRefresh: () async {
+                    context.read<GlobalSearchBloc>().add(GlobalSearchEvent.pageRefreshed());
+                  },
+                  child: SingleChildScrollView(
+                    controller: _scrollController
+                      ..addListener(() {
+                        if (_scrollController.offset == _scrollController.position.maxScrollExtent &&
+                            !state.isFetchingMoreDataForScrollDown) {
+                          context.read<GlobalSearchBloc>().add(GlobalSearchEvent.scrolledToLoadMoreItems());
+                        }
+                      }),
+                    child: SearchPeopleSection(
+                      searchValue: state.searchValue,
+                      usersForSearchQuery: state.usersForSearchQuery,
+                      isDataFetched: state.isDataFetched,
+                      isFetchingData: state.isFetchingInitialData,
+                      searchBarSearchAllPeopleTextEditingController: searchBarSearchAllPeopleTextEditingController,
+                      searchBarCleared: () {
+                        context.read<GlobalSearchBloc>().add(GlobalSearchEvent.searchValueUpdated(''));
+                      },
+                      searchValueUpdated: (newValue) {
+                        context.read<GlobalSearchBloc>().add(GlobalSearchEvent.searchValueUpdated(newValue));
+                      },
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
         );
       },
