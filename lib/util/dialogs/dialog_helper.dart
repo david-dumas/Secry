e8e6756a23c6a9aab48880bbb47cb212f6b6b37b
Twan <twanvandenbor@hotmail.com>
2022-03-22 21:17:59 +0100

    US-285: Fix showDialog close issue in DialogHelper

--- lib/util/dialogs/dialog_helper.dart
+++ lib/util/dialogs/dialog_helper.dart
@@ -5,27 +5,27 @@ import 'package:flutter/material.dart';
 class DialogHelper {
   showAlertDialog(BuildContext context,
       {required String title, required String description, AsyncCallback? extraActionOnClose}) {
-    Widget okButton = TextButton(
-      child: Text(tr('action_ok')),
-      onPressed: () async {
-        Navigator.of(context).pop();
-        if (extraActionOnClose != null) {
-          extraActionOnClose();
-        }
-      },
-    );
-
-    AlertDialog alert = AlertDialog(
-      title: Text(title),
-      content: Text(description),
-      actions: [
-        okButton,
-      ],
-    );
-
     showDialog(
       context: context,
-      builder: (BuildContext context) {
+      builder: (BuildContext dialogContext) {
+        Widget okButton = TextButton(
+          child: Text(tr('action_ok')),
+          onPressed: () async {
+            Navigator.of(dialogContext).pop();
+            if (extraActionOnClose != null) {
+              extraActionOnClose();
+            }
+          },
+        );
+
+        AlertDialog alert = AlertDialog(
+          title: Text(title),
+          content: Text(description),
+          actions: [
+            okButton,
+          ],
+        );
+
         return alert;
       },
     );
