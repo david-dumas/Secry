fe91f9bf4442da0b38cd70e9615faa9b4305df56
Twan <twanvandenbor@hotmail.com>
2022-03-25 14:49:01 +0100

    US-285: Fix email validation issue

--- lib/util/validation/email_validator.dart
+++ lib/util/validation/email_validator.dart
@@ -1,11 +1,11 @@
 import 'package:dartz/dartz.dart';
-import 'package:secry/domain/auth/email_failure.dart';
+import 'package:secry/domain/auth/validation_failures/email_failure.dart';
 
 class EmailValidator {
   Either<EmailFailure, Unit> getEmailInputFailureOrSuccessUnit({required String email}) {
     if (email.length < 1) {
       return left(EmailFailure.noEmailEntered());
-    } else if (email.contains("@")) {
+    } else if (!email.contains("@")) {
       return left(EmailFailure.emailInvalid());
     } else {
       return right(unit);
