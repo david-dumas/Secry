afd8bfc6921fd2d9bbc939a4c9607f88120cbc67
Twan <twanvandenbor@hotmail.com>
2022-03-28 19:53:19 +0200

    US-285: Delete unnecessary Tabbar Blocbuilder from LoginPage

--- lib/presentation/pages/account/login_page.dart
+++ lib/presentation/pages/account/login_page.dart
@@ -29,187 +29,182 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => getIt<SignInFormBloc>(),
-      child: BlocBuilder<TabbarBloc, TabbarState>(
-        builder: (mainContext, mainState) {
-          return BlocConsumer<SignInFormBloc, SignInFormState>(
-            listener: (context, state) {
-              state.signInFailureOrUnitOption.fold(
-                () => {},
-                (either) => either.fold(
-                  (failure) {
-                    if (state.isShowingErrorMessages) {
-                      DialogHelper().showAlertDialog(
-                        context,
-                        title: tr('warning_title_general'),
-                        description: tr(state.currentErrorMessageTag),
-                        extraActionOnClose: () async {
-                          context.read<SignInFormBloc>().add(SignInFormEvent.isShowingErrorMessagesUpdated(false));
-                        },
-                      );
-                    }
-                  },
-                  (_) {
-                    // Successfully logged in
-                    Navigator.of(context).pop();
-                  },
-                ),
-              );
-            },
-            builder: (context, state) {
-              return Scaffold(
-                backgroundColor: globalWhite,
-                appBar: GeneralAppbar(
-                  isSubpage: true,
-                ),
-                body: Center(
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding: pagePaddingZeroTop,
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                tr('account_login_title'),
-                                textAlign: TextAlign.left,
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26, color: kPrimaryColor),
-                              ),
+        create: (context) => getIt<SignInFormBloc>(),
+        child: BlocConsumer<SignInFormBloc, SignInFormState>(
+          listener: (context, state) {
+            state.signInFailureOrUnitOption.fold(
+              () => {},
+              (either) => either.fold(
+                (failure) {
+                  if (state.isShowingErrorMessages) {
+                    DialogHelper().showAlertDialog(
+                      context,
+                      title: tr('warning_title_general'),
+                      description: tr(state.currentErrorMessageTag),
+                      extraActionOnClose: () async {
+                        context.read<SignInFormBloc>().add(SignInFormEvent.isShowingErrorMessagesUpdated(false));
+                      },
+                    );
+                  }
+                },
+                (_) {
+                  // Successfully logged in
+                  Navigator.of(context).pop();
+                },
+              ),
+            );
+          },
+          builder: (context, state) {
+            return Scaffold(
+              backgroundColor: globalWhite,
+              appBar: GeneralAppbar(
+                isSubpage: true,
+              ),
+              body: Center(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: pagePaddingZeroTop,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              tr('account_login_title'),
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26, color: kPrimaryColor),
                             ),
-                            SizedBox(height: 26),
-                            TextFormField(
-                                controller: _emailTextEditController,
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    prefixIcon: Icon(Icons.email_outlined),
-                                    labelText: tr('account_email'),
-                                    suffixIcon: Visibility(
-                                      visible: state.isShowingClearEmailButton,
-                                      child: IconButton(
-                                        onPressed: () {
-                                          context.read<SignInFormBloc>().add(SignInFormEvent.emailChanged(""));
-                                          context
-                                              .read<SignInFormBloc>()
-                                              .add(SignInFormEvent.isShowingClearEmailInputToggled(false));
-                                          _emailTextEditController.clear();
-                                        },
-                                        icon: Icon(
-                                          Icons.clear,
-                                          color: kMediumGray,
-                                        ),
-                                      ),
-                                    )),
-                                onChanged: (value) =>
-                                    context.read<SignInFormBloc>().add(SignInFormEvent.emailChanged(value)),
-                                validator: (value) {
-                                  final emailValidator = EmailValidator();
-                                  final emailInputFailureOrSuccessUnit =
-                                      emailValidator.getEmailInputFailureOrSuccessUnit(email: value ?? '');
-
-                                  return emailInputFailureOrSuccessUnit.fold(
-                                    (invalidEmailError) =>
-                                        emailValidator.getErrorTextForFailure(failure: invalidEmailError),
-                                    (_) => null,
-                                  );
-                                }),
-                            SizedBox(height: 12),
-                            TextFormField(
-                                obscureText: !state.isShowingPassword,
-                                decoration: InputDecoration(
+                          ),
+                          SizedBox(height: 26),
+                          TextFormField(
+                              controller: _emailTextEditController,
+                              decoration: InputDecoration(
                                   border: OutlineInputBorder(),
-                                  prefixIcon: Icon(Icons.lock_outline),
-                                  labelText: tr('account_password'),
-                                  suffixIcon: IconButton(
-                                    icon: state.isShowingPassword ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
-                                    onPressed: () {
-                                      context
-                                          .read<SignInFormBloc>()
-                                          .add(SignInFormEvent.isShowingPasswordToggled(!state.isShowingPassword));
-                                    },
-                                  ),
-                                ),
-                                onChanged: (value) =>
-                                    context.read<SignInFormBloc>().add(SignInFormEvent.passwordChanged(value)),
-                                validator: (value) {
-                                  final passwordValidator = PasswordValidator();
-                                  final passwordInputFailureOrSuccessUnit =
-                                      passwordValidator.getPasswordInputFailureOrSuccessUnit(password: value ?? '');
-
-                                  return passwordInputFailureOrSuccessUnit.fold(
-                                    (invalidPasswordError) => passwordValidator.getErrorTextForFailure(
-                                        passwordFailure: invalidPasswordError,
-                                        deviceWidth: MediaQuery.of(context).size.width),
-                                    (_) => null,
-                                  );
-                                }),
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                TextButton(
-                                  onPressed: () {
-                                    pushNewScreen(
-                                      context,
-                                      screen: ResetPasswordPage(),
-                                      withNavBar: true,
-                                      pageTransitionAnimation: PageTransitionAnimation.cupertino,
-                                    );
-                                  },
-                                  child: Text(
-                                    tr('account_forgot_your_password'),
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      decoration: TextDecoration.underline,
-                                      fontSize: 16.0,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 20.0),
-                              child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                height: kButtonHeightMedium,
-                                child: ElevatedButton(
-                                  style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(Radius.circular(kButtonRadiusMedium)),
+                                  prefixIcon: Icon(Icons.email_outlined),
+                                  labelText: tr('account_email'),
+                                  suffixIcon: Visibility(
+                                    visible: state.isShowingClearEmailButton,
+                                    child: IconButton(
+                                      onPressed: () {
+                                        context.read<SignInFormBloc>().add(SignInFormEvent.emailChanged(""));
+                                        context
+                                            .read<SignInFormBloc>()
+                                            .add(SignInFormEvent.isShowingClearEmailInputToggled(false));
+                                        _emailTextEditController.clear();
+                                      },
+                                      icon: Icon(
+                                        Icons.clear,
+                                        color: kMediumGray,
                                       ),
                                     ),
-                                  ),
+                                  )),
+                              onChanged: (value) =>
+                                  context.read<SignInFormBloc>().add(SignInFormEvent.emailChanged(value)),
+                              validator: (value) {
+                                final emailValidator = EmailValidator();
+                                final emailInputFailureOrSuccessUnit =
+                                    emailValidator.getEmailInputFailureOrSuccessUnit(email: value ?? '');
+
+                                return emailInputFailureOrSuccessUnit.fold(
+                                  (invalidEmailError) =>
+                                      emailValidator.getErrorTextForFailure(failure: invalidEmailError),
+                                  (_) => null,
+                                );
+                              }),
+                          SizedBox(height: 12),
+                          TextFormField(
+                              obscureText: !state.isShowingPassword,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                prefixIcon: Icon(Icons.lock_outline),
+                                labelText: tr('account_password'),
+                                suffixIcon: IconButton(
+                                  icon: state.isShowingPassword ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
                                   onPressed: () {
-                                    final isValid = _formKey.currentState?.validate() ?? false;
-
-                                    if (isValid) {
-                                      _formKey.currentState?.save();
-
-                                      context.read<SignInFormBloc>().add(SignInFormEvent.signInPressed());
-                                    }
+                                    context
+                                        .read<SignInFormBloc>()
+                                        .add(SignInFormEvent.isShowingPasswordToggled(!state.isShowingPassword));
                                   },
-                                  child: Text(
-                                    tr('action_login'),
-                                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                                ),
+                              ),
+                              onChanged: (value) =>
+                                  context.read<SignInFormBloc>().add(SignInFormEvent.passwordChanged(value)),
+                              validator: (value) {
+                                final passwordValidator = PasswordValidator();
+                                final passwordInputFailureOrSuccessUnit =
+                                    passwordValidator.getPasswordInputFailureOrSuccessUnit(password: value ?? '');
+
+                                return passwordInputFailureOrSuccessUnit.fold(
+                                  (invalidPasswordError) => passwordValidator.getErrorTextForFailure(
+                                      passwordFailure: invalidPasswordError,
+                                      deviceWidth: MediaQuery.of(context).size.width),
+                                  (_) => null,
+                                );
+                              }),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  pushNewScreen(
+                                    context,
+                                    screen: ResetPasswordPage(),
+                                    withNavBar: true,
+                                    pageTransitionAnimation: PageTransitionAnimation.cupertino,
+                                  );
+                                },
+                                child: Text(
+                                  tr('account_forgot_your_password'),
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    fontSize: 16.0,
                                   ),
                                 ),
                               ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: kButtonHeightMedium,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(Radius.circular(kButtonRadiusMedium)),
+                                    ),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  final isValid = _formKey.currentState?.validate() ?? false;
+
+                                  if (isValid) {
+                                    _formKey.currentState?.save();
+
+                                    context.read<SignInFormBloc>().add(SignInFormEvent.signInPressed());
+                                  }
+                                },
+                                child: Text(
+                                  tr('action_login'),
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                                ),
+                              ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-              );
-            },
-          );
-        },
-      ),
-    );
+              ),
+            );
+          },
+        ));
   }
 }
