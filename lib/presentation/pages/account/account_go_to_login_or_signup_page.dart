acb716b516977f052b457b3a0eec5ec880099e71
Galad <galad10@live.nl>
2022-05-11 10:05:41 +0200

    US-349: add social button to account overview page

--- lib/presentation/pages/account/account_go_to_login_or_signup_page.dart
+++ lib/presentation/pages/account/account_go_to_login_or_signup_page.dart
@@ -8,9 +8,16 @@ import 'package:secry/presentation/pages/account/login_page.dart';
 import 'package:secry/presentation/pages/account/signup_page.dart';
 import 'package:secry/presentation/routes/router.gr.dart';
 import 'package:secry/presentation/widgets/bars/general_appbar.dart';
+import 'package:secry/social_media_icons_icons.dart';
 
 class AccountGoToLoginOrSignUpPage extends StatelessWidget {
   const AccountGoToLoginOrSignUpPage({Key? key}) : super(key: key);
+
+  static const Icon facebookIcon =
+      Icon(SocialMediaIcons.facebook, color: Colors.white);
+  static const Icon twitterIcon =
+      Icon(SocialMediaIcons.twitter, color: Colors.white);
+  static const Icon googleIcon = Icon(SocialMediaIcons.google);
 
   @override
   Widget build(BuildContext context) {
@@ -32,14 +39,13 @@ class AccountGoToLoginOrSignUpPage extends StatelessWidget {
                         width: MediaQuery.of(context).size.width * 0.3,
                         image: AssetImage('assets/launcher/foreground.png'),
                       ),
-                      Text(
-                        tr('account_logged_out_overview_title'),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                      ),
+                      Text(tr('account_logged_out_overview_title'),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 24)),
                       SizedBox(height: 16),
                       Text(
-                        tr('account_logged_out_overview_description'),
+                        'How well do you really know those around you? Secry is about the real world! Create a group with people you know and ask them anything!',
                         textAlign: TextAlign.center,
                         style: mainContentTextStyleMedium,
                       ),
@@ -49,9 +55,11 @@ class AccountGoToLoginOrSignUpPage extends StatelessWidget {
                         height: kButtonHeightMedium,
                         child: ElevatedButton(
                           style: ButtonStyle(
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
                               RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(kButtonRadiusMedium)),
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(kButtonRadiusMedium)),
                               ),
                             ),
                           ),
@@ -60,7 +68,8 @@ class AccountGoToLoginOrSignUpPage extends StatelessWidget {
                               context,
                               screen: SignupPage(),
                               withNavBar: true,
-                              pageTransitionAnimation: PageTransitionAnimation.cupertino,
+                              pageTransitionAnimation:
+                                  PageTransitionAnimation.cupertino,
                             );
                           },
                           child: Text(
@@ -70,24 +79,52 @@ class AccountGoToLoginOrSignUpPage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 4),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 60.0,
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          onPressed: () {
-                            pushNewScreenWithRouteSettings(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Already have an account?'),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {
+                              pushNewScreenWithRouteSettings(
+                                context,
+                                settings:
+                                    RouteSettings(name: LoginPageRoute.name),
+                                screen: LoginPage(),
+                                withNavBar: true,
+                                pageTransitionAnimation:
+                                    PageTransitionAnimation.cupertino,
+                              );
+                            },
+                            child: Text(tr('action_login')),
+                          )
+                        ],
+                      ),
+                      orDivider(),
+                      Column(
+                        children: [
+                          continueWithSocialMediaButton(
                               context,
-                              settings: RouteSettings(name: LoginPageRoute.name),
-                              screen: LoginPage(),
-                              withNavBar: true,
-                              pageTransitionAnimation: PageTransitionAnimation.cupertino,
-                            );
-                          },
-                          child: Text(tr('action_login')),
-                        ),
+                              googleIcon,
+                              Colors.white,
+                              'Continue with Google',
+                              Colors.grey),
+                          continueWithSocialMediaButton(
+                              context,
+                              twitterIcon,
+                              Colors.blue,
+                              'Continue with Twitter',
+                              Colors.white),
+                          continueWithSocialMediaButton(
+                              context,
+                              facebookIcon,
+                              Color.fromRGBO(63, 95, 175, 1),
+                              'Continue with Facebook',
+                              Colors.white),
+                        ],
                       ),
                       verticalSafetyScrollOffset
                     ],
@@ -100,4 +137,57 @@ class AccountGoToLoginOrSignUpPage extends StatelessWidget {
       },
     );
   }
+}
+
+Widget orDivider() {
+  return Row(children: <Widget>[
+    Expanded(
+      child: Container(
+          margin: const EdgeInsets.only(left: 10.0, right: 20.0),
+          child: Divider(thickness: 2.0, height: 36)),
+    ),
+    Text("OR", style: TextStyle(fontSize: 18.0)),
+    Expanded(
+      child: new Container(
+          margin: const EdgeInsets.only(left: 20.0, right: 10.0),
+          child: Divider(thickness: 2.0, height: 36)),
+    ),
+  ]);
+}
+
+Widget continueWithSocialMediaButton(
+  BuildContext context,
+  Icon socialMediaIcon,
+  Color buttonColor,
+  String continueWithText,
+  Color textColor,
+) {
+  return Container(
+    margin: EdgeInsets.only(bottom: 10.0),
+    width: MediaQuery.of(context).size.width,
+    height: kButtonHeightMedium,
+    child: OutlinedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(buttonColor),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.all(Radius.circular(kButtonRadiusMedium)),
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            socialMediaIcon,
+            Padding(
+              padding: EdgeInsets.only(left: 15.0),
+              child: Text(
+                continueWithText,
+                style: TextStyle(color: textColor, fontSize: 16.0),
+              ),
+            )
+          ],
+        )),
+  );
 }
