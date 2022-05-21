e329ff66f25f03d586a64786cd0474c5b5c6ee1c
Twan <twanvandenbor@hotmail.com>
2022-05-21 21:14:00 +0200

    US-353: Scroll new question into view with question textfield focused

--- lib/presentation/pages/add_survey/widgets/question_text_input.dart
+++ lib/presentation/pages/add_survey/widgets/question_text_input.dart
@@ -2,19 +2,20 @@ import 'dart:math';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:secry/domain/surveys/model/question.dart';
 
 import '../../../../constants.dart';
 
-class QuestionTextInput extends StatelessWidget {
+class QuestionTextInput extends StatefulWidget {
   final Question question;
   final GlobalKey<FormState> _formKey;
 
   final int maximumQuestionLength;
   final Function(String newValue) questionTextUpdated;
 
-  const QuestionTextInput(
+  QuestionTextInput(
       {Key? key,
       required this.question,
       required GlobalKey<FormState> formKey,
@@ -24,21 +25,39 @@ class QuestionTextInput extends StatelessWidget {
         super(key: key);
 
   @override
+  State<QuestionTextInput> createState() => _QuestionTextInputState();
+}
+
+class _QuestionTextInputState extends State<QuestionTextInput> {
+  final focusNode = FocusNode();
+
+  @override
+  void initState() {
+    SchedulerBinding.instance?.addPostFrameCallback((Duration _) {
+      FocusScope.of(context).requestFocus(focusNode);
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
+      key: widget._formKey,
       child: TextFormField(
-        key: Key(question.id),
-        initialValue: question.text,
+        key: Key(widget.question.id),
+        initialValue: widget.question.text,
+        focusNode: focusNode,
         autofocus: false,
         autocorrect: false,
         keyboardType: TextInputType.multiline,
+        scrollPadding: EdgeInsets.only(bottom: 175),
         maxLines: 5,
         minLines: 1,
         decoration: InputDecoration(
           fillColor: globalWhite,
           filled: true,
-          suffixText: "${max(0, maximumQuestionLength - question.text.length)}",
+          suffixText: "${max(0, widget.maximumQuestionLength - widget.question.text.length)}",
           hintText: tr('survey_question_hint_text'),
           contentPadding: EdgeInsets.all(20.0),
           border: OutlineInputBorder(
@@ -50,7 +69,7 @@ class QuestionTextInput extends StatelessWidget {
           ),
         ),
         inputFormatters: [
-          LengthLimitingTextInputFormatter(maximumQuestionLength),
+          LengthLimitingTextInputFormatter(widget.maximumQuestionLength),
         ],
         validator: (String? value) {
           if (value == null) {
@@ -58,13 +77,13 @@ class QuestionTextInput extends StatelessWidget {
           }
           if (value.isEmpty) {
             return tr('warning_field_is_empty');
-          } else if (value.length > maximumQuestionLength) {
+          } else if (value.length > widget.maximumQuestionLength) {
             return tr('warning_too_many_characters');
           }
           return null;
         },
         onChanged: (newValue) {
-          questionTextUpdated(newValue);
+          widget.questionTextUpdated(newValue);
         },
       ),
     );
