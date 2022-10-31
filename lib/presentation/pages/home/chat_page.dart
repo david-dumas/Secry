5f950016e8c14223760e4ae0ae79302837032371
justiandevs <justiandev@gmail.com>
2022-10-31 12:24:10 +0100

    US_579: Bottom bar for chat page

--- lib/presentation/pages/home/chat_page.dart
+++ lib/presentation/pages/home/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:secry/presentation/widgets/bars/general_appbar.dart';
 
@@ -18,12 +19,47 @@ class ChatPage extends StatelessWidget {
         backgroundColor: globalWhite,
         isShowingBottomBorder: true,
       ),
+      bottomNavigationBar: Container(
+          padding: MediaQuery.of(context).viewInsets,
+          color: kLightGray,
+          child: Container(
+              padding: EdgeInsets.symmetric(vertical: 12),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+                        filled: true,
+                        fillColor: globalWhite,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(kButtonRadiusXxs),
+                          borderSide: BorderSide.none
+                        ),
+                        hintText: tr('chat_page_send_a_message'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  IconButton(
+                      onPressed: () {},
+                      splashRadius: 0.1,
+                      icon: Icon(Icons.camera_alt, color: kMediumGrayV2, size: 24.0))
+                ],
+              ))
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: pagePaddingAllSides,
-          child: Text("chat id: ${chatId}"),
+          child: Column(
+            children: [
+              Text('message 1'),
+              Text('message 2')
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
